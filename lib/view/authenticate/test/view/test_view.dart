a43bc2c1d8bd31bc5344d852a506436de790de9f
Gurkan Mutlu <gurkanmutllu@gmail.com>
2022-10-09 21:14:23 +0300

    s0e3 completed

--- lib/view/authenticate/test/view/test_view.dart
+++ lib/view/authenticate/test/view/test_view.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:fluttermvvmtemplate/core/base/state/base_state.dart';
 import 'package:fluttermvvmtemplate/core/base/view/base_widget.dart';
+
+import '../viewmodel/test_view_model.dart';
 
 class TestView extends StatefulWidget {
   const TestView({super.key});
@@ -10,11 +13,35 @@ class TestView extends StatefulWidget {
 }
 
 class _TestViewState extends BaseState<TestView> {
+  late TestViewModel viewModel;
+
   @override
   Widget build(BuildContext context) {
-    return BaseView(
-      viewModel: "",
-      onPageBuilder: (context, value) => const Text('data'),
+    return BaseView<TestViewModel>(
+      viewModel: TestViewModel(),
+      onModelReady: ((model) {
+        viewModel = model;
+      }),
+      onPageBuilder: (context, value) => scaffoldBody,
+    );
+  }
+
+  Widget get scaffoldBody => Scaffold(
+        floatingActionButton: floatingActionButtonNumberIncrement,
+        body: textNumber,
+      );
+
+  FloatingActionButton get floatingActionButtonNumberIncrement {
+    return FloatingActionButton(
+      onPressed: () => viewModel.incrementNumber(),
+    );
+  }
+
+  Widget get textNumber {
+    return Observer(
+      builder: (BuildContext context) => Text(
+        viewModel.number.toString(),
+      ),
     );
   }
 }
---
+++ lib/view/authenticate/test/viewmodel/test_view_model.dart
@@ -0,0 +1,18 @@
+import 'package:mobx/mobx.dart';
+
+part 'test_view_model.g.dart';
+
+class TestViewModel = _TestViewModelBase with _$TestViewModel;
+
+abstract class _TestViewModelBase with Store {
+  @observable
+  int number = 0;
+
+  @computed
+  bool get isEven => number % 2 == 0;
+
+  @action
+  void incrementNumber() {
+    number++;
+  }
+}
