16785a61c0e665bfb90c9fc96bc2f54507444eb3
Gurkan Mutlu <gurkanmutllu@gmail.com>
2022-10-12 02:03:53 +0300

    s0e4 finishedd

--- lib/view/authenticate/test/viewmodel/test_view_model.dart
+++ lib/view/authenticate/test/viewmodel/test_view_model.dart
@@ -1,10 +1,25 @@
+import 'package:flutter/cupertino.dart';
 import 'package:mobx/mobx.dart';
+import 'package:provider/provider.dart';
+
+import '../../../../core/base/model/base_view_model.dart';
+import '../../../../core/constants/enums/app_theme_enum.dart';
+import '../../../../core/init/notifier/theme_notifier.dart';
 
 part 'test_view_model.g.dart';
 
+// ignore: library_private_types_in_public_api
 class TestViewModel = _TestViewModelBase with _$TestViewModel;
 
-abstract class _TestViewModelBase with Store {
+abstract class _TestViewModelBase with Store, BaseViewModel {
+  @override
+  void setContext(BuildContext context) {
+    contextt = context;
+  }
+
+  @override
+  void init() {}
+
   @observable
   int number = 0;
 
@@ -14,5 +29,9 @@ abstract class _TestViewModelBase with Store {
   @action
   void incrementNumber() {
     number++;
+  }
+
+  void changeTheme() {
+    Provider.of<ThemeNotifier>(contextt!, listen: false).changeValue(AppThemes.DARK);
   }
 }
