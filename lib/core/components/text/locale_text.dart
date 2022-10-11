11c9b401073cb80b0e216bb054b581bf8a7e6d0d
Gurkan Mutlu <gurkanmutllu@gmail.com>
2022-10-12 02:01:30 +0300

    s0e4 finished

---
+++ lib/core/components/text/locale_text.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import '../../extension/string_extension.dart';
+
+class LocaleText extends StatelessWidget {
+  final String value;
+
+  const LocaleText({super.key, required this.value});
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(value.locale);
+  }
+}
