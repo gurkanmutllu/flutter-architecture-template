f4d8e3cd6c73b6993642557ee6d6c5377d760564
Gurkan Mutlu <gurkanmutllu@gmail.com>
2022-10-12 04:01:59 +0300

    s0e5 finished

--- lib/core/init/notifier/provider_list.dart
+++ lib/core/init/notifier/provider_list.dart
@@ -1,4 +1,8 @@
+import 'package:provider/provider.dart';
 import 'package:provider/single_child_widget.dart';
+
+import '../navigation/navigation_service.dart';
+import 'theme_notifier.dart';
 
 class ApplicationProvider {
   static ApplicationProvider? _instance;
@@ -10,6 +14,11 @@ class ApplicationProvider {
   ApplicationProvider._init();
 
   List<SingleChildWidget> singleItems = [];
-  List<SingleChildWidget> dependItems = [];
+  List<SingleChildWidget> dependItems = [
+    ChangeNotifierProvider(
+      create: (context) => ThemeNotifier(),
+    ),
+    Provider.value(value: NavigationService.instance),
+  ];
   List<SingleChildWidget> uiChangesItems = [];
 }
